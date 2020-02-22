8490eb8ee0a3adb46e50defda24081e7034e0943
FrootLoops78 <4011138@g.highlineschools.org>
2020-02-22 11:35:04 -0800

    fixed style issues

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,8 +1,8 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* the project. */
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
@@ -23,11 +23,10 @@ import frc.robot.subsystems.Flywheel;
 import frc.robot.subsystems.Intake;
 
 /**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a "declarative" paradigm, very little robot logic should
- * actually be handled in the {@link Robot} periodic methods (other than the
- * scheduler calls). Instead, the structure of the robot (including subsystems,
- * commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
@@ -40,7 +39,6 @@ public class RobotContainer {
   private final Intake m_intake = new Intake();
   // private final Drive m_driveCommand = new Drive(m_chassis);
   private final ReadEncoder m_encoderReadCommand = new ReadEncoder(m_chassis);
-
   public static final Joystick leftJoystick = new Joystick(Constants.leftJoystick);
   public static final Joystick rightJoystick = new Joystick(Constants.rightJoystick);
   public static final Button normalButton = new JoystickButton(leftJoystick, 2);
@@ -52,21 +50,17 @@ public class RobotContainer {
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
-
-    
     // Configure the button bindings
     configureButtonBindings();
     // m_chassis.setDefaultCommand(m_driveCommand);
     m_chassis.setDefaultCommand(m_encoderReadCommand);
-
-    
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be
-   * created by instantiating a {@link GenericHID} or one of its subclasses
-   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
-   * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
     normalButton.whileHeld(new ShootFlywheel(m_normalFlywheel));
