da433f063893e013fc23bf71cf4bb5ebad1e2a47
FrootLoops78 <4011138@g.highlineschools.org>
2020-02-29 17:33:13 -0800

    establishing roboRIO connection part 2
    almost there hhh

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,12 +9,10 @@ package frc.robot;
 
 import edu.wpi.first.networktables.EntryListenerFlags;
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -23,18 +21,10 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 
-
- 
-
-
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-  private NetworkTableEntry m_distanceEntry;
-  private NetworkTableEntry m_rotationEntry;
-  private double m_y = 0;
-  private double m_x = 0;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -43,11 +33,30 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
+    // Get the default instance of NetworkTables
     NetworkTableInstance inst = NetworkTableInstance.getDefault();
+
+    // Get a reference to the subtable called "datatable"
     NetworkTable table = inst.getTable("datatable");
-    m_distanceEntry = table.getEntry("X");
-    m_rotationEntry = table.getEntry("Y");
+    System.out.println("it's working now lmao");
+    // Creates entires by getting entries.
+    table.getEntry("distance entry").forceSetDouble(0.0);
+    table.getEntry("rotation entry").forceSetDouble(0.0);
+
+    table.addEntryListener("distance entry", (distanceTable, key, entry, value, flags) -> {
+
+      System.out.println("setting distance");
+
+    }, EntryListenerFlags.kLocal | EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
+    table.addEntryListener("rotation entry", (rotationTable, key, entry, value, flags) -> {
+
+      System.out.println("setting degrees");
+    }, EntryListenerFlags.kLocal | EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
+
+    inst.startClientTeam(4461);
   }
+
+
 
   /**
    * This function is called every robot packet, no matter the mode. Use this for items like
@@ -109,11 +118,6 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-    // **This makes it so we can change any values in teleop. */
-    m_distanceEntry.setDouble(m_x);
-    m_rotationEntry.setDouble(m_y);
-    m_x += 0.05;
-    m_y += 1;
   }
 
   /**
@@ -121,38 +125,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    // get the default instance of NetworkTables
-    NetworkTableInstance inst = NetworkTableInstance.getDefault();
-
-    // get a reference to the subtable called "datatable"
-    NetworkTable table = inst.getTable("datatable");
-
-    // get a reference to key in "datatable" called "Y"
-
-    NetworkTableEntry m_rotationEntry = table.getEntry("Y");
-    inst.startClientTeam(190);
-
-    // add an entry listener for changed values of "X", the lambda ("->" operator)
-    // defines the code that should run when "X" changes
-    table.addEntryListener("X", (NetworkTable, key, entry, value, flags) -> {
-      System.out.println("X changed value: " + value.getValue());
-    }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
-
-    // add an entry listener for changed values of "Y", the lambda ("->" operator)
-    // defines the code that should run when "Y" changes
-    m_rotationEntry.addListener(event -> {
-      System.out.println("Y changed value: " + m_rotationEntry.getValue());
-    }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
-
-    try {
-      Thread.sleep(10000);
-    } catch (InterruptedException ex) {
-      System.out.println("Interrupted");
-      Thread.currentThread().interrupt();
-      return;
-      }
-    }
-    
+  }
 
   @Override
   public void testInit() {
@@ -166,5 +139,4 @@ public class Robot extends TimedRobot {
   @Override
   public void testPeriodic() {
   }
-
 }
