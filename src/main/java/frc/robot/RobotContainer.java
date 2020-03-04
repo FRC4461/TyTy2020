ea6ee921ef48bd06a0d8f8970e8450a0aa50263f
DaffodilPlate67 <vincerosane@outlook.com>
2020-03-03 19:42:01 -0800

    This is the final working drivebase
    All ID's and naming are fixed as well as the controllers

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Button;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.Drive;
 import frc.robot.commands.ReadEncoder;
 import frc.robot.commands.RunConveyor;
 import frc.robot.commands.RunIntake;
@@ -32,7 +33,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final double m_normalFlywheelSpeed = -0.5;
   private final double m_turboFlywheelSpeed = -1;
-  private final Chassis m_chassis = new Chassis();
+  private static final Chassis m_chassis = new Chassis();
   private final Flywheel m_normalFlywheel = new Flywheel(m_normalFlywheelSpeed);
   private final Flywheel m_turboFlywheel = new Flywheel(m_turboFlywheelSpeed);
   private final Conveyor m_conveyor = new Conveyor();
@@ -46,14 +47,16 @@ public class RobotContainer {
   public static final Button conveyorButton = new JoystickButton(rightJoystick, 2);
   public static final Button intakeButton = new JoystickButton(rightJoystick, 1);
 
+  public static final Drive m_driveCommand = new Drive(m_chassis);
+
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
-    // m_chassis.setDefaultCommand(m_driveCommand);
-    m_chassis.setDefaultCommand(m_encoderReadCommand);
+    m_chassis.setDefaultCommand(m_driveCommand);
+    // m_chassis.setDefaultCommand(m_encoderReadCommand);
   }
 
   /**
